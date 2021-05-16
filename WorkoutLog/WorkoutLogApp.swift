fdd6f4f334c04b3181423205c6caaded61672a10
蘇健豪 <jenhausu@icloud.com>
2021-05-16 21:00:15 +0800

    Initial Commit

---
+++ WorkoutLog/WorkoutLogApp.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutLogApp.swift
+//  WorkoutLog
+//
+//  Created by 蘇健豪 on 2021/5/16.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutLogApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
