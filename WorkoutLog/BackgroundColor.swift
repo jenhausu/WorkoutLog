3acb7e0e387c874301f914a7ba6c9a2aad9ce6e2
蘇健豪 <jenhausu@icloud.com>
2021-12-05 15:58:51 +0800

    feat: Background color modifier

--- WorkoutLog/BackgroundColor.swift
+++ WorkoutLog/BackgroundColor.swift
@@ -5,4 +5,12 @@
 //  Created by 蘇健豪 on 2021/12/5.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    func backgroundColor(color: Color) -> some View {
+        self
+            .background(color)
+    }
+}
+
